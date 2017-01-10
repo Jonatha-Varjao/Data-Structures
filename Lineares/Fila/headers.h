47d569ec522ecd26571d1c5a79561be43d071843
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-01-09 21:02:17 -0300

    Add files via upload

--- Lineares/Fila/headers.h
+++ Lineares/Fila/headers.h
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <malloc.h>
 
 //estrutura com os dados
 typedef struct no
@@ -15,16 +16,19 @@ typedef struct queue
   node *ultimo;
 }fila;
 
-// Funções da Fila//
+// Funções da Fila// 
 
 //criar fila
 void createQeue(fila *queue);
+
+//Get NO
+node *getNO(int valor);
 
 //inserir
 void queue(fila *queue, int data);
 
 //remover
-void deQueue(fila *queue, int data);
+void deQueue(fila *queue);
 
 //imprimir fila
 void printQueue(fila *queue);
@@ -32,3 +36,60 @@ void printQueue(fila *queue);
 //busca na fila
 void searchQueue(fila *queue, int data);
 
+
+void createQeue(fila *queue)
+{
+	queue->primeiro = queue->ultimo = NULL;
+}
+
+node *getNO(int valor)
+{
+	node *new = (node*)malloc(sizeof(node));
+	if(new != NULL)
+	{
+		new->prox = NULL;
+		new->info = valor;
+	}
+	return new;
+}
+
+void queue(fila *queue, int data)
+{
+	node *new = getNO(data);
+	if(queue->primeiro == NULL)
+	{
+		queue->primeiro = queue->ultimo = new;
+		//queue->primeiro->prox = queue->ultimo->prox = NULL;
+	}
+	else
+	{
+		queue->ultimo->prox = new;
+		queue->ultimo = new;
+	}
+
+}
+
+void printQueue(fila *queue)
+{
+	node *aux = queue->primeiro;
+	if(queue!= NULL){
+		while(aux != NULL)
+		{
+			printf("[%d] ", aux->info );
+			aux = aux->prox;
+		}
+	}
+}
+
+void deQueue(fila *queue)
+{
+	if (queue->primeiro == NULL){printf("FILA VAZIA\n"); }
+	else
+	{
+		node *aux;
+		aux = queue->primeiro;
+		queue->primeiro = queue->primeiro->prox;
+		printf("Removendo [%d] da Fila\n",aux->info);
+		free(aux);
+	}
+}
--- Lineares/Fila/main.c
+++ Lineares/Fila/main.c
@@ -3,6 +3,16 @@
 int main(int argc, char const *argv[])
 {
 	//TODO CODE HERE//
+	fila *Queue;
+	//deQueue(&Queue);
+	createQeue(&Queue);
+	queue(&Queue, 3);
+	queue(&Queue,5);
+	queue(&Queue,7);
+	//printQueue(&Queue);
+	deQueue(&Queue);
+	printQueue(&Queue);
 
+	printf("HELLO WORLD\n");
 	return 0;
 }
