a573eb1690461abc8507626c6e529f02d183eee0
LPatiele <lo.patiele@gmail.com>
2017-02-02 14:08:57 -0200

    "i don't know what to put in here....."

--- Lineares/Listas/Lista Duplamente Encadeada/headers.h
+++ Lineares/Listas/Lista Duplamente Encadeada/headers.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h>
+
 
 typedef struct node
 {
@@ -8,12 +10,12 @@ typedef struct node
 
 typedef struct lista
 {
-	node *head, *tail;
+	node *head,*tail;
 	int qtd;
 }lista;
 
 //lista vazia
-void emptyList(lista l);
+int emptyList(lista *l);
 
 //iniciar a lsita
 void createLista(lista *l);
@@ -28,11 +30,81 @@ void insertBody(lista *l, int data);
 void insertTail(lista *l, int data);
 
 //inserir ordenado
-void ordenedInsert(list *l, int data);
+void ordenedInsert(lista *l, int data);
 
 //ordenar lista
 void sortList(lista *l);
 
+void printLista(lista *l);
+
+void createLista(lista *l)
+{
+	l->head = l->tail = NULL;
+}
+
+int emptyList(lista *l)
+{
+	if(l->head == NULL)return 1;
+	else return 0;
+}
+
+node *getNO(int data)
+{
+	node *new = (node*)malloc(sizeof(node));
+	if(new!=NULL)
+	{
+		new->data = data;
+		new->prev = new->next = NULL;
+	}else
+		printf("ALLOCATION ERROR");
+}
+
+void insertHead(lista *l, int data)
+{
+	if(l==NULL)
+	{
+		node *new = (node*)malloc(sizeof(node));
+		new = getNO(data);
+		l->head = l->tail = new;
+	}else
+	{
+		node *new = (node*)malloc(sizeof(node));
+		new = getNO(data);
+		new->next = l->head;
+		l->head->prev = new;
+		l->head = new;
+	}
+}
+void insertTail(lista *l, int data);
+
+
+void printLista(lista *l)
+{
+	
+	if(emptyList(l) == 0)
+	{
+		node *aux = l->head;
+		while(aux!=NULL)
+		{
+			printf("%d->", aux->data);
+			aux = aux->next;
+		}
+	}
+	else printf("EMPTY LIST\n");
+}
+
+void printListaReverse(lista *l)
+{
+	if(emptyList(l)== 0)
+	{
+		node *aux = l->tail;
+		while(aux!=NULL)
+		{
+			printf("<-%d ", aux->data);
+			aux = aux->prev;
+		}
+	}else printf("EMPTY LIST\n");
+}
 //remover
 
 //remover inicio
--- Lineares/Listas/Lista Duplamente Encadeada/main.c
+++ Lineares/Listas/Lista Duplamente Encadeada/main.c
@@ -1,7 +1,13 @@
 #include "headers.h"
-int main(int argc, char const *argv[])
+int main()//int argc, char const *argv[])
 {
-	//TODO CODE HERE//
+	lista L;
+	createLista(&L);
+	insertHead(&L,30);
+	//insertHead(&L,50);
+	//insertHead(&L,60);
+	printLista(&L);
+	printListaReverse(&L);
 
 	return 0;
 }
